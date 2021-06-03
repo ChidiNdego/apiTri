d966c90498018be08e085f32f3cef631c519adb5
Chidi Ndego <chidi.ndego@stanbicibtc.com>
2021-06-03 21:20:33 +0100

    extras folder created

--- connection.py
+++
@@ -1,8 +0,0 @@
-import pyodbc
-server = '10.234.18.152'
-database = 'RSVREDONG'
-# database = 'RSVRRealtimeFinacleNG'
-username = 'EDOUSER'
-password = 'Stanbic_123#'
-
-db_conn = pyodbc.connect('Driver={SQL Server};Server='+server+';DATABASE='+database+';UID='+username+';PWD='+password+';')
